741bf6ab6b78585da82b24bf482ad7bb0afae29e
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-06-05 00:56:49 -0300

    feat: implementação da estratégia jwt e dos guards

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { LocalStrategy } from './local.strategy';
   }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
